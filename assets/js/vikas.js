9c43db38906a34baf1bf0bd2f4029cdcc711d75a
Sai Vikas Reddy Musku <saivikasreddy989@gmail.com>
2023-09-28 18:20:48 -0500

    Adding resume

--- assets/js/vikas.js
+++ assets/js/vikas.js
@@ -1,18 +1,3 @@
-/*!
-=========================================================
-* JohnDoe Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// smooth scroll
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
 
@@ -55,6 +40,11 @@ $(window).on("load", function() {
     });
 });
 
+document.getElementById("resume-button").addEventListener("click", redirectFunction);
+
+function redirectFunction(){
+    window.location.href = "https://github.com/saivikas27/SaiVikasReddyMusku/tree/main/assets/resume/Vikas-Resume.pdf";
+}
 
 // google maps
 function initMap() {
